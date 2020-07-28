abb5171ba0e3a5e525c5db6f40eff6ffdcf6b994
nosam1998 <nosam1998@gmail.com>
2020-07-28 02:54:34 -0700

    It works! Just have to add styling now

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var questionJsonObj;
 var onQuestionNum = 0; // Increment this every time a question is answered
 var score = 0;
 var questionDifficulty = ["easy", "medium", "hard"];
+var questionCount = 10
 
-$.getJSON('https://opentdb.com/api.php?amount=10&category=18&type=multiple', function (data) {
+
+$.getJSON('https://opentdb.com/api.php?amount=' + questionCount + '&category=18&type=multiple', function (data) {
     // JSON result in `data` variable
     if (data.response_code === 0) {
         console.log("Got response!")
@@ -28,10 +30,13 @@ function populateHtmlWithQuestion() {
     // Based on the variable onQuestionNum populate the question data
     var question = document.getElementById("question");
     var answer_options = document.getElementsByClassName("answer");
+    var scoreElem = document.getElementById("score");
     var answer_arr = questionJsonObj[onQuestionNum].incorrect_answers;
+
     answer_arr.push(questionJsonObj[onQuestionNum].correct_answer);
 
     question.innerText = questionJsonObj[onQuestionNum].question; // Set the #question to the question text
+    scoreElem.innerText = score;
 
     for (var i = 0; i < answer_arr.length; i++) {
         answer_options[i].innerText = unescape(answer_arr[i]);
@@ -47,19 +52,23 @@ function getAnswerText(obj) {
 }
 
 function checkAnswerText(answerText) {
-    if (questionJsonObj[onQuestionNum].correct_answer == answerText) {
-        // The user answered the question correctly so do the proper logic here
-        // Points added are based on the question difficulty
-        var pointsToAdd = questionDifficulty.indexOf(questionJsonObj[onQuestionNum].difficulty) + 1
-        score += pointsToAdd;
-        console.log("Question #" + onQuestionNum + " answered correctly!");
+
+    if (onQuestionNum < questionCount - 1) {
+        if (questionJsonObj[onQuestionNum].correct_answer == answerText) {
+            // The user answered the question correctly so do the proper logic here
+            // Points added are based on the question difficulty
+            var pointsToAdd = questionDifficulty.indexOf(questionJsonObj[onQuestionNum].difficulty) + 1
+            score += pointsToAdd;
+            console.log("Question #" + onQuestionNum + " answered correctly!");
+        } else {
+            // The user answered the question incorrectly so do the proper logic here
+            console.log("Question #" + onQuestionNum + " answered incorrectly!");
+        }
+
         onQuestionNum += 1;
         populateHtmlWithQuestion();
     } else {
-        // The user answered the question incorrectly so do the proper logic here
-        console.log("Question #" + onQuestionNum + " answered incorrectly!");
-        onQuestionNum += 1;
-        populateHtmlWithQuestion();
+        alert("You got a score of: " + score + "! In a total of " + questionCount + " questions!")
     }
 
 }
