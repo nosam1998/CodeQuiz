5574dc270cbf093a3d5c4878c1182e2f41f16fa2
nosam1998 <nosam1998@gmail.com>
2020-07-28 02:34:25 -0700

    Questions now autopopulate and are dynamic.

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var questionDifficulty = ["easy", "medium", "hard"];
 
 $.getJSON('https://opentdb.com/api.php?amount=10&category=18&type=multiple', function (data) {
     // JSON result in `data` variable
-    questionJsonObj = data;
+    if (data.response_code === 0) {
+        console.log("Got response!")
+        questionJsonObj = data.results;
+        populateHtmlWithQuestion();
+    } else {
+        alert("Error getting question data from API!")
+    }
 });
 
 
@@ -20,39 +26,38 @@ function setScoreLocalStorage() {
 
 function populateHtmlWithQuestion() {
     // Based on the variable onQuestionNum populate the question data
-    var questionObj = questionJsonObj[onQuestionNum];
     var question = document.getElementById("question");
     var answer_options = document.getElementsByClassName("answer");
-    var answer_arr = questionObj.incorrect_answers;
-    answer_arr.push(questionObj.correct_answer);
-    shuffle(answer_arr);
+    var answer_arr = questionJsonObj[onQuestionNum].incorrect_answers;
+    answer_arr.push(questionJsonObj[onQuestionNum].correct_answer);
 
-    question.innerText = questionObj.question; // Set the #question to the question text
+    question.innerText = questionJsonObj[onQuestionNum].question; // Set the #question to the question text
 
     for (var i = 0; i < answer_arr.length; i++) {
-        answer_options[i].innerText = answer_arr[i];
+        answer_options[i].innerText = unescape(answer_arr[i]);
     }
-
+    onQuestionNum += 1;
 }
 
 
-function getAnswerText(this) {
-    var answerText = this.innerText;
+function getAnswerText(obj) {
+    var answerText = obj.innerText;
+    console.log(answerText);
     checkAnswerText(answerText);
 }
 
 function checkAnswerText(answerText) {
-    var questionObj = questionJsonObj[onQuestionNum];
-
-    if (questionObj.correctAnswern === answerText) {
+    if (questionJsonObj[onQuestionNum].correct_answer === answerText) {
         // The user answered the question correctly so do the proper logic here
         // Points added are based on the question difficulty
-        var pointsToAdd = questionDifficulty.indexOf(questionObj.difficulty) + 1
+        var pointsToAdd = questionDifficulty.indexOf(questionJsonObj[onQuestionNum].difficulty) + 1
         score += pointsToAdd;
         console.log("Question #" + onQuestionNum + " answered correctly!");
-
+        populateHtmlWithQuestion();
     } else {
         // The user answered the question incorrectly so do the proper logic here
         console.log("Question #" + onQuestionNum + " answered incorrectly!");
+        populateHtmlWithQuestion();
     }
+
 }
