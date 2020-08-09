78f300a0661587e42ffe4df96ffd2e64a283129b
nosam1998 <nosam1998@gmail.com>
2020-08-08 20:42:09 -0700

    Fixed answer button positions

--- js/script.js
+++ js/script.js
@@ -28,106 +28,108 @@ var clearScoresBtn = document.getElementById("clearScores");
 // Question Object/Array
 var questionObj = {
     "results": [{
-        "difficulty": "easy",
-        "question": "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
-        "correct_answer": "1000",
-        "incorrect_answers": ["512", "1024", "500"]
-    }, {
-        "difficulty": "medium",
-        "question": "What does AD stand for in relation to Windows Operating Systems? ",
-        "correct_answer": "Active Directory",
-        "incorrect_answers": ["Alternative Drive", "Automated Database", "Active Department"]
-    }, {
-        "difficulty": "easy",
-        "question": "The programming language 'Swift' was created to replace what other programming language?",
-        "correct_answer": "Objective-C",
-        "incorrect_answers": ["C#", "Ruby", "C++"]
-    }, {
-        "difficulty": "medium",
-        "question": "What was the name given to Android 4.3?",
-        "correct_answer": "Jelly Bean",
-        "incorrect_answers": ["Lollipop", "Nutella", "Froyo"]
-    }, {
-        "difficulty": "easy",
-        "question": "In web design, what does CSS stand for?",
-        "correct_answer": "Cascading Style Sheet",
-        "incorrect_answers": ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
-    }, {
-        "difficulty": "medium",
-        "question": "While Apple was formed in California, in which western state was Microsoft founded?",
-        "correct_answer": "New Mexico",
-        "incorrect_answers": ["Washington", "Colorado", "Arizona"]
-    }, {
-        "difficulty": "medium",
-        "question": "Which one of these is not an official development name for a Ubuntu release?",
-        "correct_answer": "Mystic Mansion",
-        "incorrect_answers": ["Trusty Tahr", "Utopic Unicorn", "Wily Werewolf"]
-    }, {
-        "difficulty": "hard",
-        "question": "Which of these was the name of a bug found in April 2014 in the publicly available OpenSSL cryptography library?",
-        "correct_answer": "Heartbleed",
-        "incorrect_answers": ["Shellshock", "Corrupted Blood", "Shellscript"]
-    }, {
-        "difficulty": "medium",
-        "question": "What is the correct term for the metal object in between the CPU and the CPU fan within a computer system?",
-        "correct_answer": "Heat Sink",
-        "incorrect_answers": ["CPU Vent", "Temperature Decipator", "Heat Vent"]
-    }, {
-        "difficulty": "easy",
-        "question": "What does LTS stand for in the software market?",
-        "correct_answer": "Long Term Support",
-        "incorrect_answers": ["Long Taco Service", "Ludicrous Transfer Speed", "Ludicrous Turbo Speed"]
-    }, {
-        "difficulty": "hard",
-        "question": "Who is the original author of the realtime physics engine called PhysX?",
-        "correct_answer": "NovodeX",
-        "incorrect_answers": ["Ageia", "Nvidia", "AMD"]
-    }, {
-        "difficulty": "hard",
-        "question": "What is the name of the process that sends one qubit of information using two bits of classical information?",
-        "correct_answer": "Quantum Teleportation",
-        "incorrect_answers": ["Super Dense Coding", "Quantum Entanglement", "Quantum Programming"]
-    }, {
-        "difficulty": "medium",
-        "question": "What is the main CPU is the Sega Mega Drive \/ Sega Genesis?",
-        "correct_answer": "Motorola 68000",
-        "incorrect_answers": ["Zilog Z80", "Yamaha YM2612", "Intel 8088"]
-    }, {
-        "difficulty": "hard",
-        "question": "What does the International System of Quantities refer 1024 bytes as?",
-        "correct_answer": "Kibibyte",
-        "incorrect_answers": ["Kylobyte", "Kilobyte", "Kelobyte"]
-    }, {
-        "difficulty": "hard",
-        "question": "What vulnerability ranked #1 on the OWASP Top 10 in 2013?",
-        "correct_answer": "Injection ",
-        "incorrect_answers": ["Broken Authentication", "Cross-Site Scripting", "Insecure Direct Object References"]
-    }, {
-        "difficulty": "hard",
-        "question": "Who invented the 'Spanning Tree Protocol'?",
-        "correct_answer": "Radia Perlman",
-        "incorrect_answers": ["Paul Vixie", "Vint Cerf", "Michael Roberts"]
-    }, {
-        "difficulty": "easy",
-        "question": "Which programming language shares its name with an island in Indonesia?",
-        "correct_answer": "Java",
-        "incorrect_answers": ["Python", "C", "Jakarta"]
-    }, {
-        "difficulty": "easy",
-        "question": "In computing, what does MIDI stand for?",
-        "correct_answer": "Musical Instrument Digital Interface",
-        "incorrect_answers": ["Musical Interface of Digital Instruments", "Modular Interface of Digital Instruments", "Musical Instrument Data Interface"]
-    }, {
-        "difficulty": "hard",
-        "question": "Which of these is not a layer in the OSI model for data communications?",
-        "correct_answer": "Connection Layer",
-        "incorrect_answers": ["Application Layer", "Transport Layer", "Physical Layer"]
-    }, {
-        "difficulty": "medium",
-        "question": "The teapot often seen in many 3D modeling applications is called what?",
-        "correct_answer": "Utah Teapot",
-        "incorrect_answers": ["Pixar Teapot", "3D Teapot", "Tennessee Teapot"]
-    }]
+            "difficulty": "easy",
+            "question": "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
+            "correct_answer": "1000",
+            "incorrect_answers": ["512", "1024", "500"]
+        },
+        // {
+        //     "difficulty": "medium",
+        //     "question": "What does AD stand for in relation to Windows Operating Systems? ",
+        //     "correct_answer": "Active Directory",
+        //     "incorrect_answers": ["Alternative Drive", "Automated Database", "Active Department"]
+        // }, {
+        //     "difficulty": "easy",
+        //     "question": "The programming language 'Swift' was created to replace what other programming language?",
+        //     "correct_answer": "Objective-C",
+        //     "incorrect_answers": ["C#", "Ruby", "C++"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "What was the name given to Android 4.3?",
+        //     "correct_answer": "Jelly Bean",
+        //     "incorrect_answers": ["Lollipop", "Nutella", "Froyo"]
+        // }, {
+        //     "difficulty": "easy",
+        //     "question": "In web design, what does CSS stand for?",
+        //     "correct_answer": "Cascading Style Sheet",
+        //     "incorrect_answers": ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "While Apple was formed in California, in which western state was Microsoft founded?",
+        //     "correct_answer": "New Mexico",
+        //     "incorrect_answers": ["Washington", "Colorado", "Arizona"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "Which one of these is not an official development name for a Ubuntu release?",
+        //     "correct_answer": "Mystic Mansion",
+        //     "incorrect_answers": ["Trusty Tahr", "Utopic Unicorn", "Wily Werewolf"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "Which of these was the name of a bug found in April 2014 in the publicly available OpenSSL cryptography library?",
+        //     "correct_answer": "Heartbleed",
+        //     "incorrect_answers": ["Shellshock", "Corrupted Blood", "Shellscript"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "What is the correct term for the metal object in between the CPU and the CPU fan within a computer system?",
+        //     "correct_answer": "Heat Sink",
+        //     "incorrect_answers": ["CPU Vent", "Temperature Decipator", "Heat Vent"]
+        // }, {
+        //     "difficulty": "easy",
+        //     "question": "What does LTS stand for in the software market?",
+        //     "correct_answer": "Long Term Support",
+        //     "incorrect_answers": ["Long Taco Service", "Ludicrous Transfer Speed", "Ludicrous Turbo Speed"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "Who is the original author of the realtime physics engine called PhysX?",
+        //     "correct_answer": "NovodeX",
+        //     "incorrect_answers": ["Ageia", "Nvidia", "AMD"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "What is the name of the process that sends one qubit of information using two bits of classical information?",
+        //     "correct_answer": "Quantum Teleportation",
+        //     "incorrect_answers": ["Super Dense Coding", "Quantum Entanglement", "Quantum Programming"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "What is the main CPU is the Sega Mega Drive \/ Sega Genesis?",
+        //     "correct_answer": "Motorola 68000",
+        //     "incorrect_answers": ["Zilog Z80", "Yamaha YM2612", "Intel 8088"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "What does the International System of Quantities refer 1024 bytes as?",
+        //     "correct_answer": "Kibibyte",
+        //     "incorrect_answers": ["Kylobyte", "Kilobyte", "Kelobyte"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "What vulnerability ranked #1 on the OWASP Top 10 in 2013?",
+        //     "correct_answer": "Injection ",
+        //     "incorrect_answers": ["Broken Authentication", "Cross-Site Scripting", "Insecure Direct Object References"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "Who invented the 'Spanning Tree Protocol'?",
+        //     "correct_answer": "Radia Perlman",
+        //     "incorrect_answers": ["Paul Vixie", "Vint Cerf", "Michael Roberts"]
+        // }, {
+        //     "difficulty": "easy",
+        //     "question": "Which programming language shares its name with an island in Indonesia?",
+        //     "correct_answer": "Java",
+        //     "incorrect_answers": ["Python", "C", "Jakarta"]
+        // }, {
+        //     "difficulty": "easy",
+        //     "question": "In computing, what does MIDI stand for?",
+        //     "correct_answer": "Musical Instrument Digital Interface",
+        //     "incorrect_answers": ["Musical Interface of Digital Instruments", "Modular Interface of Digital Instruments", "Musical Instrument Data Interface"]
+        // }, {
+        //     "difficulty": "hard",
+        //     "question": "Which of these is not a layer in the OSI model for data communications?",
+        //     "correct_answer": "Connection Layer",
+        //     "incorrect_answers": ["Application Layer", "Transport Layer", "Physical Layer"]
+        // }, {
+        //     "difficulty": "medium",
+        //     "question": "The teapot often seen in many 3D modeling applications is called what?",
+        //     "correct_answer": "Utah Teapot",
+        //     "incorrect_answers": ["Pixar Teapot", "3D Teapot", "Tennessee Teapot"]
+        // }
+    ]
 }
 
 
